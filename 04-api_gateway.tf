b5e101e142709c9dfea1c4a43a1eef3ce15f1346
Matt Todd <matt.c.todd@gmail.com>
2018-04-08 14:33:47 -0500

    pulled into module

--- 04-api_gateway.tf
+++ 04-api_gateway.tf
@@ -1,40 +1,18 @@
-
-# API Gateway
 resource "aws_api_gateway_rest_api" "api" {
   name = "coffeemonkey"
 }
 
-resource "aws_api_gateway_resource" "movie-resource" {
+module "api_endpoint" {
+  source = "./modules/api"
+
+  root_resource_id = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  api_id = "${aws_api_gateway_rest_api.api.id}"
   path_part = "movies"
-  parent_id = "${aws_api_gateway_rest_api.api.root_resource_id}"
-  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-}
+  method = "GET"
+  lambda_arn = "${aws_lambda_function.lambda.arn}"
 
-resource "aws_api_gateway_method" "method" {
-  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
-  resource_id   = "${aws_api_gateway_resource.movie-resource.id}"
-  http_method   = "GET"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "integration" {
-  rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
-  resource_id             = "${aws_api_gateway_resource.movie-resource.id}"
-  http_method             = "${aws_api_gateway_method.method.http_method}"
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = "arn:aws:apigateway:${var.myregion}:lambda:path/2015-03-31/functions/${aws_lambda_function.lambda.arn}/invocations"
-}
-
-# Lambda
-resource "aws_lambda_permission" "apigw_lambda" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.lambda.arn}"
-  principal     = "apigateway.amazonaws.com"
-
-  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  source_arn = "arn:aws:execute-api:${var.myregion}:${var.accountId}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.movie-resource.path}"
+  myregion = "${var.myregion}"
+  accountId = "${var.accountId}"
 }
 
 resource "aws_lambda_function" "lambda" {
@@ -46,7 +24,7 @@ resource "aws_lambda_function" "lambda" {
   source_code_hash = "${base64sha256(file("lambda.zip"))}"
 }
 
-# IAM
+
 resource "aws_iam_role" "role" {
   name = "myrole"
 
@@ -65,29 +43,6 @@ resource "aws_iam_role" "role" {
   ]
 }
 POLICY
-}
-
-resource "aws_api_gateway_deployment" "deployment" {
-  depends_on = ["aws_api_gateway_integration.integration"]
-  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-  stage_name = "dev"
-}
-
-resource "aws_api_gateway_stage" "stage" {
-  stage_name = "prod"
-  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-  deployment_id = "${aws_api_gateway_deployment.deployment.id}"
-}
-
-resource "aws_api_gateway_method_settings" "s" {
-  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-  stage_name  = "${aws_api_gateway_stage.stage.stage_name}"
-  method_path = "${aws_api_gateway_resource.movie-resource.path_part}/${aws_api_gateway_method.method.http_method}"
-
-  settings {
-    metrics_enabled = true
-  logging_level = "INFO"
-  }
 }
 
 
---
+++ modules/api/variables.tf
@@ -0,0 +1,12 @@
+
+variable "api_id" {}
+variable "root_resource_id" {}
+
+variable "path_part" {}
+variable "method" {}
+variable "lambda_arn" {}
+
+
+variable "myregion" {}
+variable "accountId" {}
+
